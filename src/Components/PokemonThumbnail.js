20f59270cb5af1f4b7972cd24571d97fa3a318ec
Alexandrov Ilya <59890241+sanyaburba@users.noreply.github.com>
2021-12-29 22:16:10 +0300

    Update PokemonThumbnail.js

--- src/Components/PokemonThumbnail.js
+++ src/Components/PokemonThumbnail.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import Description from "./Description";
 
-const PokemonThumbnail = ({id, name, image, type, height, weight, stat1, stat2, stat3, stat4, stat5, stat6, bs1, bs2, bs3, bs4, bs5, bs6, evoChain}) => {
+const PokemonThumbnail = ({id, name, image, type, height, weight, stat1, stat2, stat3, stat4, stat5, stat6, bs1, bs2, bs3, bs4, bs5, bs6}) => {
 
     const style = `thumb-container ${type}`;
     const [show, setShow] = useState(false);
@@ -9,7 +9,6 @@ const PokemonThumbnail = ({id, name, image, type, height, weight, stat1, stat2, 
         <div className={style}>
             <div className="number">
                 <small>#0{id}</small>
-                <small>{evoChain}</small>
             </div>
             <img src={image} alt={name}/>
             <div className="detail-wrapper">
@@ -35,7 +34,6 @@ const PokemonThumbnail = ({id, name, image, type, height, weight, stat1, stat2, 
                         posbs4={bs4}
                         posbs5={bs5}
                         posbs6={bs6}
-                        evoChain={evoChain}
                     />
                 ) : (
                     <></>
